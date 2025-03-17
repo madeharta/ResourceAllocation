5bde33c14805ddb0202802e5706f220957998e7b
Made Harta <made.harta@gmail.com>
2025-03-17 16:00:06 +0700

    Third Commit

--- BaseStation.py
+++ BaseStation.py
@@ -2,13 +2,15 @@ import numpy as np
 
 RAND_MAX = 32787
 
+
 class BaseStation:
-    def __init__(self, id, type, noUE, position):
+    def __init__(self, id, type, noUE, position, radius):
         self.id = id
         self.type = type
         self.noUE = noUE
         self.position = position
         self.fading_bs_to_ms = np.random(noUE,)*RAND_MAX
+        self.radius = radius
 
     def get_position(self):
         return self.position
@@ -22,4 +24,5 @@ class BaseStation:
     def get_fading_info(self, ueID):
         return self.fading_bs_to_ms[ueID]
     
-    
+    def get_radius(self):
+        return self.radius
---
+++ MobileStation.py
@@ -0,0 +1,60 @@
+import numpy as np
+
+LOG_NORMAL_MEAN = 0
+LOG_NORMAL_STD_MS = 8
+DECORRELATED_DISTANCE = 50
+
+
+class MobileStation:
+    def __init__(self, id, pos, speed, mn):
+        self.id = id
+        self.pos = pos
+        self.shadowingDB = np.random.normal(LOG_NORMAL_MEAN, LOG_NORMAL_STD_MS)
+        self.prevPosUpdateShadowing = pos
+        self.speed = speed
+        self.mn = mn
+
+    def get_id(self):
+        return self.id
+
+    def get_pos(self):
+        return self.pos
+
+    def set_mn(self, mn):
+        self.mn = mn
+
+    def update_pos(self, pos):
+        self.pos = pos
+
+    def update_bc(self, bcId):
+        self.bcId = bcId        
+
+    def _get_distance(self, pos1, pos2):
+        return np.sqrt(np.power(pos1[0]-pos2[0],2)+np.power(pos1[1]-pos2[1],2))
+
+    def update_shadowing(self):
+        movingDistance = self._get_distance(self.prevPosUpdateShadowing, self.pos)
+
+        largestDB		= 2.*np.abs(self.shadowingDB)
+        smallestDB		= -2.*np.abs(self.shadowingDB)
+
+        Factor_c = np.exp(-1.0 * (movingDistance/DECORRELATED_DISTANCE) * np.log(2.0))
+        self.shadowingDB = Factor_c * self.shadowingDB + np.sqrt(1.-Factor_c*Factor_c) * np.random.normal(LOG_NORMAL_MEAN, LOG_NORMAL_STD_MS) #getGaussian();
+
+        if (self.shadowingDB > largestDB):
+            self.shadowingDB = largestDB
+        if (self.shadowingDB < smallestDB): 
+            self.shadowingDB = smallestDB
+ 
+        self.prevPosUpdateShadowing = self.pos
+
+    def auto_update_pos(self, deltaTime):
+        movingDistance = deltaTime * self.speed * 1000/3600		# speed is in km/h so it needs to be converted to m/s
+        newX = self.pos[0] + (movingDistance*np.cos(self.getMovingDirection()))
+        newY = self.pos[1] - (movingDistance*np.sin(self.getMovingDirection()))
+
+        if (this.bs.getPosition().distance(new PositionHolder(newX, newY)) > Simulator.CELL_RADIUS): 	
+            # if the user move out to the cell, place it again in cell randomly
+            self.update_pos(this.bs.getRandomPointCircle(CELL_RADIUS))
+        else:
+            self.update_pos(np.array(newX, newY))
